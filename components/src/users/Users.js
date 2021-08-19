60a4bdb328c1baa009b9cfdd21e42e4172ba0fd0
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-19 21:38:49 +0200

    Defined the Users class component

---
+++ components/src/users/Users.js
@@ -0,0 +1,11 @@
+// Class-based component
+import React, {Component} from 'react';
+
+class Users extends Component {
+    render() {
+        return <div>test</div>
+    }
+}
+
+// Exporting the class component
+export default Users;
