8286665697b3c22562153c1ab1f578c45e0b9940
Shuvadarshan Bhual <sbhual1998@gmail.com>
2021-08-23 00:09:34 +0200

    Added the makeMeYounger function

--- components/src/users/Users.js
+++ components/src/users/Users.js
@@ -17,10 +17,20 @@ class Users extends Component {
 		],
 		title: "Users list"
 	};
-			
+		
+	makeMeYounger = () => {
+		const newState = this.state.users.map((user) => {
+			const tempUser = user;
+			tempUser.age -= 10;
+			return tempUser;
+		})
+		
+		
+	}
 	
     render() {
         return (<div>
+        <button onClick={this.makeMeYounger}>Make us 10 years younger</button> <br/>
         {this.state.title}
         <User age={this.state.users[0].age}>{this.state.users[0].name}</User>
         <User age={this.state.users[1].age}>{this.state.users[1].name}</User>
